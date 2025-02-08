b146936d229676a1b6d37344d44fe07a0b6923a5
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2025-02-08 16:44:50 -0600

    Ascending and Descending sort

--- include/sortingAlgs.h
+++ include/sortingAlgs.h
@@ -1,10 +1,10 @@
 #ifndef SORTINGALGS_H
 #define SORTINGALGS_H
 
-enum class Sort
+enum class SortOrder
 {
-    ascending,
-    descending
+    Ascending,
+    escending
 };
 
 template <typename T>
@@ -16,35 +16,42 @@ void swap(T &a, T &b)
 }
 
 template <typename T>
-void bubbleSort(T arr[], int size)
+void bubbleSort(T arr[], int size, SortOrder order = SortOrder::Ascending)
 {
     int maxElement, index;
 
     for (maxElement = size - 1; maxElement > 0; maxElement--)
+    {
         for (index = 0; index < maxElement; index++)
-            if (arr[index] > arr[index + 1])
-                swap(arr[index], arr[index + 1]);
+        {
+            bool swap = (order == SortOrder::Ascending)
+                            ? (arr[i] > arr[index + 1])
+                            : (arr[index] < arr[index + 1]);
+
+            if (swap)
+                swap(arr[index], arr[index + 1])
+        };
+    }
 }
 
 template <typename T>
-void selectionSort(T arr[], int size)
+void selectionSort(T arr[], int size, SortOrder order = SortOrder::Ascending)
 {
-    int minIndex, minValue;
-
     for (int start = 0; start < (size - 1); start++)
     {
-        minIndex = start;
-        minValue = arr[start];
+        int selIndex = start;
 
         for (int i = start + 1; i < size; i++)
         {
-            if (arr[i] < minValue)
-            {
-                minValue = arr[i];
-                minIndex = i;
-            }
+            bool swap = (order == SortOrder::Ascending)
+                            ? (arr[i] > arr[selIndex])
+                            : (arr[i] < arr[selIndex]);
+
+            if (swap)
+                selIndex = i;
         }
-        swap(arr[minIndex], arr[start]);
+        if (selIndex != start)
+            swap(arr[start], arr[selIndex]);
     }
 }
 
