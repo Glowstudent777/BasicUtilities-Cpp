54a6db0c9b7ffe1604b1f9d967de6fc3c5d1a5c9
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2025-02-08 17:00:42 -0600

    Add a dualSort for sorting 2d arrays?

--- include/sortingAlgs.h
+++ include/sortingAlgs.h
@@ -55,4 +55,28 @@ void selectionSort(T arr[], int size, SortOrder order = SortOrder::Ascending)
     }
 }
 
+template <typename T>
+void dualSort(T arr1[], T arr2[], int size, SortOrder order = SortOrder::Ascending)
+{
+    for (int start = 0; start < (size - 1); start++)
+    {
+        int selIndex = start;
+
+        for (int i = start + 1; i < size; i++)
+        {
+            bool swapE = (order == SortOrder::Ascending)
+                             ? (arr1[i] < arr1[selIndex])
+                             : (arr1[i] > arr1[selIndex]);
+
+            if (swapE)
+                selIndex = i;
+        }
+        if (selIndex != start)
+        {
+            swap(arr1[start], arr1[selIndex]);
+            swap(arr2[start], arr2[selIndex]);
+        }
+    }
+}
+
 #endif
